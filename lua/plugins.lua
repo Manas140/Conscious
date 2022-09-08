bfde67a3b5f76c254864ce1639e7a123ec20d75f
datsfilipe <datsfilipe.dev@gmail.com>
2022-09-08 00:33:22 -0300

    Add bootstrapping to automatically install and setup packer.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,105 +1,106 @@
-local install_path = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-
-if fn.empty(fn.glob(install_path)) > 0 then
-  print('Installing Packer')
-  cmd(':!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
 end
 
-cmd [[ packadd packer.nvim ]]
+local packer_bootstrap = ensure_packer()
 
-require('packer').startup {
-  function()
-    use 'wbthomason/packer.nvim'
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  -- In Buffer ( Auto )
+  use { 
+    'norcalli/nvim-colorizer.lua',
+    config = function()
+      require('colorizer').setup()
+    end
+  }
+  use {
+    'lukas-reineke/indent-blankline.nvim',
+    config = function()
+      require('plugins.indent_blankline')
+    end
+  }
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    requires = { 'nathom/filetype.nvim' },
+    config = function()
+      require('plugins.treesitter')
+    end
+  }
 
-    -- In Buffer ( Auto )
-    use { 
-      'norcalli/nvim-colorizer.lua',
-      config = function()
-        require('colorizer').setup()
-      end,
-    }
-    use {
-      'lukas-reineke/indent-blankline.nvim',
-      config = function()
-        require('plugins.indent_blankline')
-      end,
-    }
-    use {
-      'nvim-treesitter/nvim-treesitter',
-      requires = { 'nathom/filetype.nvim' },
-      config = function()
-        require('plugins.treesitter')
-      end
-    }
+  -- UI
+  use 'RRethy/nvim-base16'
+  use {
+    'kyazdani42/nvim-tree.lua',
+    config = function()
+      require('plugins.nvim-tree')
+    end
+  }
+  use { 
+    'lewis6991/gitsigns.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('plugins.gitsigns')
+    end
+  }
+  use { 
+    'nvim-telescope/telescope.nvim',
+    requires = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require('plugins.telescope')
+    end
+  }
+  use {
+    'akinsho/bufferline.nvim',
+    config = function()
+      require('plugins.bufferline')
+    end
+  }
 
-    -- UI
-    use 'RRethy/nvim-base16'
-    use {
-      'kyazdani42/nvim-tree.lua',
-      config = function()
-        require('plugins.nvim-tree')
-      end,
-    }
-    use { 
-      'lewis6991/gitsigns.nvim',
-      requires = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        require('plugins.gitsigns')
-      end,
-    }
-    use { 
-      'nvim-telescope/telescope.nvim',
-      requires = { 'nvim-lua/plenary.nvim' },
-      config = function()
-        require('plugins.telescope')
-      end,
-    }
-    use {
-      'akinsho/bufferline.nvim',
-      config = function()
-        require('plugins.bufferline')
-      end,
-    }
+  -- Completion, LSP
+  use {
+    'windwp/nvim-autopairs',
+    config = function()
+      require('nvim-autopairs').setup()
+    end
+  }
+  use {
+    'hrsh7th/nvim-cmp',
+    requires = { 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline'},
+    config = function()
+      require('plugins.cmp')
+    end
+  }
+  use {
+    'numToStr/Comment.nvim',
+    config = function()
+      require('Comment').setup()
+    end
+  }
 
-    -- Completion, LSP
-    use {
-      'windwp/nvim-autopairs',
-      config = function()
-        require('nvim-autopairs').setup()
-      end
-    }
-    use {
-      'hrsh7th/nvim-cmp',
-      requires = { 'hrsh7th/cmp-buffer', 'hrsh7th/cmp-path', 'hrsh7th/cmp-cmdline'},
-      config = function()
-        require('plugins.cmp')
-      end,
-    }
-    use {
-      'numToStr/Comment.nvim',
-      config = function()
-        require('Comment').setup()
-      end
-    }
+  use {
+    'neovim/nvim-lspconfig',
+    requires = { 'hrsh7th/cmp-nvim-lsp', 'williamboman/nvim-lsp-installer' },
+    config = function()
+      require("plugins.lsp")
+    end
+  }
 
-    use {
-      'neovim/nvim-lspconfig',
-      requires = { 'hrsh7th/cmp-nvim-lsp', 'williamboman/nvim-lsp-installer' },
-      config = function()
-        require("plugins.lsp")
-      end
-    }
-
-    -- Snippets
-    use {
-      'L3MON4D3/LuaSnip',
-      after = 'nvim-cmp',
-      requires = { 'saadparwaiz1/cmp_luasnip', 'rafamadriz/friendly-snippets' },
-      config = function()
-        require('plugins.luasnip')
-      end,
-    }
-  end,
+  -- Snippets
+  use {
+    'L3MON4D3/LuaSnip',
+    after = 'nvim-cmp',
+    requires = { 'saadparwaiz1/cmp_luasnip', 'rafamadriz/friendly-snippets' },
+    config = function()
+      require('plugins.luasnip')
+    end
+  }
 
   config = {
     display = {
@@ -107,6 +108,10 @@ require('packer').startup {
         return require('packer.util').float { border = 'single' }
       end,
       prompt_border = 'single',
-    },
-  },
-}
+    }
+  }
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
